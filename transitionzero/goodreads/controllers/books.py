1727a4e5719853aac05dd18a09e8c551bea72816
Azizjon Abdurasulov <Azizjon.Abdurasulov@ventionteams.com>
2025-12-18 18:42:19 +0500

    Add endpoints for books

--- transitionzero/goodreads/controllers/books.py
+++ transitionzero/goodreads/controllers/books.py
@@ -1,25 +1,37 @@
-from typing import List
-
+from fastapi import HTTPException
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
-from transitionzero.goodreads.models import database
+from transitionzero.goodreads.models.database import Book
 
-def search(db: Session):
-    # Your code here! 
-    pass
 
-def get(db: Session):
-    # Your code here! 
-    pass
+class ServiceBooks:
 
-def post(db: Session):
-    # Your code here! 
-    pass
+    def search(self, db: Session, user_id: int):
+        obj = db.execute(select(Book).where(Book.id == user_id)).scalar()
+        if not obj:
+            raise HTTPException(status_code=404, detail="Book not found")
 
-def patch(db: Session):
-    # Your code here! 
-    pass
+        return obj
 
-def delete(db: Session):
-    # Your code here! 
-    pass
+    def get(self, db: Session):
+        return db.execute(select(Book)).scalars().all()
+
+    def post(self, db: Session, data: dict):
+        new_book = Book(**data)
+        db.add(new_book)
+        db.commit()
+        db.refresh(new_book)
+        return new_book
+
+    def patch(self, db: Session, book_id: int, data: dict):
+        db.execute(update(Book).where(Book.id == book_id).values(**data))
+        db.commit()
+
+
+    def delete(self, db: Session, book_id: int):
+        obj = self.search(db, book_id)
+        db.delete(obj)
+        db.commit()
+
+service_books = ServiceBooks()
---
+++ transitionzero/goodreads/views/books.py
@@ -0,0 +1,43 @@
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from transitionzero.goodreads.controllers.books import ServiceBooks, service_books
+from transitionzero.goodreads.models.database import get_db
+from transitionzero.goodreads.views.schemas import BookSchema, BookSchemaBase
+
+router = APIRouter()
+
+@router.get("/books", response_model=list[BookSchema])
+def get_books(
+        session: Session = Depends(get_db)):
+    return service_books.get(session)
+
+@router.get("/search/{book_id}", response_model=BookSchema)
+def get_book_by_id(
+        book_id: int,
+        session: Session = Depends(get_db)
+):
+    return service_books.search(session, book_id)
+
+@router.post("/books", response_model=BookSchema)
+def create_book(
+        book: BookSchemaBase,
+        session: Session = Depends(get_db)
+):
+    return service_books.post(session, book.model_dump())
+
+@router.patch("/books/{book_id}")
+def patch_book(
+        book_id: int,
+        book: BookSchemaBase,
+        session: Session = Depends(get_db)
+):
+    return service_books.patch(session, book_id, book.model_dump(exclude_unset=True))
+
+@router.delete("/books/{book_id}")
+def delete_book(
+        book_id: int,
+        session: Session = Depends(get_db)
+):
+    return service_books.delete(session, book_id)
